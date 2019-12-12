6f599d9aff6aa28028602027c0966bf65126d138
Parshav <parshav@live.com>
2019-12-11 20:29:25 -0600

    looping through stream types

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate cpal;
 use cpal::traits::HostTrait;
 use cpal::traits::DeviceTrait;
 use cpal::traits::EventLoopTrait;
+use cpal::{StreamData, UnknownTypeOutputBuffer};
 
 fn main() {
     println!("Hello, world!");
@@ -16,8 +17,28 @@ fn main() {
     let stream_id = event_loop.build_output_stream(&device, &format).expect("Error in creating stream id");
 
     event_loop.play_stream(stream_id).expect("Error in play stream.");
-    event_loop.run(move |s_id, s_r|{
+    event_loop.run(move |_s_id, result|{
 
-        println!("SID : {:?} SR", s_id);
+        let data = match result {
+            Ok(d) => d,
+            Err(e) => {
+                println!("Error in data {:?}", e);
+                return;
+            }
+        };
+
+        // I think matched to what was provided to stream;
+        match data {
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut _buffer) } => {
+                println!("Inside u16");
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut _buffer) } => {
+                println!("Inside i16")
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut _buffer) } => {
+                println!("Inside f32")
+            },
+            _ => (),
+        };
     });
 }
