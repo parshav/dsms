8356577d6c79fe64d9466c5be5089ed016bb8da6
Parshav <parshav@live.com>
2019-12-11 20:14:07 -0600

    Some loop is there.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate cpal;
 use cpal::traits::HostTrait;
 use cpal::traits::DeviceTrait;
+use cpal::traits::EventLoopTrait;
 
 fn main() {
     println!("Hello, world!");
@@ -9,7 +10,14 @@ fn main() {
     let event_loop = host.event_loop();
     let device = host.default_output_device().expect("No device found");
 
-    let format = device.default_output_format().expect("No format found");
+    let format = device.default_output_format().expect("No format "); // Prints sample rate and format.
     println!("Default! : {:?}", format);
-    
+
+    let stream_id = event_loop.build_output_stream(&device, &format).expect("Error in creating stream id");
+
+    event_loop.play_stream(stream_id).expect("Error in play stream.");
+    event_loop.run(move |s_id, s_r|{
+
+        println!("SID : {:?} SR", s_id);
+    });
 }
