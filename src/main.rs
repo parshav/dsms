8f89733431a9c811abf4b43bd087a08c7dc2093c
Parshav <parshav@live.com>
2019-12-15 17:03:51 -0600

    Function for custom device added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate cpal;
 use cpal::traits::HostTrait;
 use cpal::traits::DeviceTrait;
 use cpal::traits::EventLoopTrait;
+use cpal::Host;
+use cpal::Device;
 use cpal::{StreamData, UnknownTypeOutputBuffer};
 
 fn main() {
@@ -10,9 +12,11 @@ fn main() {
     let host = cpal::default_host();
     let event_loop = host.event_loop();
     let device = host.default_output_device().expect("No device found");
+    println!("Default Device : {:?}", device.name());
 
     let format = device.default_output_format().expect("No format "); // Prints sample rate and format.
-    println!("Default! : {:?}", format);
+
+    let device = custom_device(&host);
 
     let stream_id = event_loop.build_output_stream(&device, &format).expect("Error in creating stream id");
 
@@ -36,9 +40,28 @@ fn main() {
                 println!("Inside i16")
             },
             StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut _buffer) } => {
-                println!("Inside f32")
+                println!("Inside i32")
             },
             _ => (),
         };
     });
+}
+
+fn list_devices(host: &Host) {
+
+    let devices = host.output_devices().expect("Error in multiple output devices");
+
+    for d in devices {
+        println!("Name of found device : {:?}", d.name());
+    }
+
+}
+
+fn custom_device(host: &Host) -> Device {
+    let device = host.output_devices()
+        .expect("Error")
+        .find( |x| x.name().expect("Error in name").contains( "pulse"))
+        .expect("Error on finding the device boi");
+    println!("DDD : {:?}", device.name());
+    return device;
 }
